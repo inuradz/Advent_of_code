344a16f23062bab3b8f870862c4a5575ab726ae7
Inura De Zoysa <inuradezoysa@gmail.com>
2019-12-12 22:03:53 +1100

    Added logging to the intcode interpreter

--- intcode/src/main/java/intcode/IntCodeVM.java
+++ intcode/src/main/java/intcode/IntCodeVM.java
@@ -3,7 +3,9 @@ package intcode;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.*;
-
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.*;
 /**
  * A new VM to handle the intcode assembly with other features
  */
@@ -16,6 +18,8 @@ public class IntCodeVM {
     Integer instructionPointer;
     Stack<Integer> data_stack;
 
+    static Logger log = LogManager.getLogger(IntCodeVM.class);
+
     IntCodeVM(InputStream input, PrintStream output, Collection<Integer> instructions){
         this.input = input;
         in = new Scanner(input);
@@ -23,19 +27,40 @@ public class IntCodeVM {
         memory = new ArrayList<Integer>(instructions);
         instructionPointer = 0;
         data_stack = new Stack<Integer>();
+        log.debug("Start Memory state  {}",memoryStateString());
         this.run();
+        log.debug("Finish Memory state {}",memoryStateString());
     }
 
     List<Integer> getState(){
         return memory;
     }
 
+    String memoryStateString(){
+        StringBuilder sb = new StringBuilder();
+        Boolean start = true;
+        sb.append("[");
+        Integer counter = 0;
+        for(Integer i: memory){
+            if(!start){
+                sb.append(", ");
+            }
+            sb.append(String.format("%s:%s",counter,i));
+            counter++;
+            start = false;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
     void loadNumber(Integer address){
+        log.info("Loading Direct Value {}",memory.get(address));
         data_stack.push(memory.get(address));
         instructionPointer++;
     }
 
     void loadNumberFromAddress(Integer address){
+        log.info("Loading Value From Memory Address {} which contains {}",memory.get(address),memory.get(memory.get(address)));
         data_stack.push(memory.get(memory.get(address)));
         instructionPointer++;
     }
@@ -62,32 +87,36 @@ public class IntCodeVM {
      */
     boolean interpretInstruction(){
         String instruction = String.format("%05d",memory.get(instructionPointer));
-        System.out.println(instruction);
         instructionPointer++;
         switch (instruction.substring(3)){
             case "01":
                 //This is an addition operation
+                log.info("Running add instruction | {} | at location {}",instruction,instructionPointer);
                 loadNextIntegersSequence(instruction,2);
                 loadNumber(this.instructionPointer);
                 memory.set(data_stack.pop(),data_stack.pop()+data_stack.pop());
                 break;
             case "02":
                 //This is a multiplication operation
+                log.info("Running multiply instruction | {} | at location {}",instruction,instructionPointer);
                 loadNextIntegersSequence(instruction,2);
                 loadNumber(this.instructionPointer);
                 memory.set(data_stack.pop(),data_stack.pop()*data_stack.pop());
                 break;
             case "03":
                 //Read in an integer
+                log.info("Reading in an integer | {} | at location {}",instruction,instructionPointer);
                 loadNumber(this.instructionPointer);
                 memory.set(data_stack.pop(),in.nextInt());
                 break;
             case "04":
                 //Outputs an integer
-                loadNumber(this.instructionPointer);
-                out.print(memory.get(data_stack.pop()));
+                log.info("Outputting an integer | {} | at location {}",instruction,instructionPointer);
+                loadNextIntegersSequence(instruction,1);
+                out.print(data_stack.pop());
                 break;
             case "99":
+                log.info("STOP instruction | {} | at location {}",instruction,instructionPointer);
                 return true;
             default:
                 throw new IllegalArgumentException("Unknow argument" + instruction);
@@ -98,6 +127,7 @@ public class IntCodeVM {
     public void run(){
         while(!interpretInstruction()){
             //Do nothing, this can be used to show other stuff is need be
+            log.debug("Memory state  {}",memoryStateString());
         }
     }
 }
--- intcode/src/test/java/intcode/VMTest.java
+++ intcode/src/test/java/intcode/VMTest.java
@@ -73,6 +73,12 @@ public class VMTest {
     }
 
     @Test
+    public void testMultiplyDirectReversed(){
+        IntCodeVM s = new IntCodeVM(System.in,System.out,Arrays.asList(1102,23,3,3,99));
+        Assert.assertEquals(s.getState(),Arrays.asList(1102,23,3,69,99));
+    }
+
+    @Test
     public void testMultiplyMixed(){
         IntCodeVM s = new IntCodeVM(System.in,System.out,Arrays.asList(102,10,5,3,99,8));
         Assert.assertEquals(s.getState(),Arrays.asList(102,10,5,80,99,8));
